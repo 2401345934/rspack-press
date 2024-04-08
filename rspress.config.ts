3ee3eaddda0254213dfaa64baf0afb8106788ab4
alan.xiao <52618511+2401345934@users.noreply.github.com>
2024-04-08 19:33:15 +0800

    更新配置

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,20 +3,14 @@ import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
-  title: 'Rspress',
-  description: 'Rspack-based Static Site Generator',
+  title: 'Alan Xiao',
   icon: "https://raw.githubusercontent.com/2401345934/picgo/master/imgrspress-icon.png",
   logo: {
     light: "https://raw.githubusercontent.com/2401345934/picgo/master/imgrspress-light-logo.png",
     dark: "https://raw.githubusercontent.com/2401345934/picgo/master/imgrspress-dark-logo.png",
   },
   builderConfig: {
-    dev: {
-        assetPrefix: true,
-    },
-    output: {
-      assetPrefix: './',
-    },
+ 
   },
   themeConfig: {
     socialLinks: [
